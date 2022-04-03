d292373591bf9d9a3766d400a39eed8378e3e5a4
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2022-04-03 17:58:11 +0900

    before_credential

--- src/main/java/com/cavnas/co/controller/CanvasController.java
+++ src/main/java/com/cavnas/co/controller/CanvasController.java
@@ -4,16 +4,25 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+import com.cavnas.co.service.GoogleService;
+import com.google.protobuf.ByteString;
+
+@RestController
 @RequestMapping("canvas")
 public class CanvasController {
+	
+	
+	@Autowired
+	private GoogleService googleService;
 	
 	@PostMapping("/post")
 	@ResponseBody
@@ -21,10 +30,27 @@ public class CanvasController {
 			@RequestParam(value = "file", required = false) MultipartFile file// upload file
 	) throws IOException {
 		
-		Map<String , Object> result = new HashMap<String,Object>();
-	
+		byte [] byteArray =  file.getBytes();
+		ByteString imgBytes  = ByteString.copyFrom(byteArray);
+		System.out.println("file ::" +  imgBytes);
 		
+		googleService.detectText(imgBytes);
+		//ByteString imgBytes = file.getBytes();
+		Map<String , Object> result = new HashMap<String,Object>();
+		
+		result.put("test", "test!!");
+		result.put("status","200" );
 		return result;
 	}
-
+	
+	
+	//JAVA Controller source
+	//MultipartHttpServletRequest 타입으로 받는다.
+	@PostMapping(value="/test")
+	@ResponseBody
+	public void test(@RequestBody Map<String, Object> param ) throws Exception{
+	     String ImgReader = param.get("image")+ ""; 
+	     googleService.detectTextFromImage(ImgReader);
+		
+	}
 }
---
+++ src/main/java/com/cavnas/co/service/GoogleService.java
@@ -0,0 +1,101 @@
+package com.cavnas.co.service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
+
+@Service
+public class GoogleService {
+	public static void detectText() throws IOException {
+// TODO(developer): Replace these variables before running the sample.
+		String filePath = "path/to/your/image/file.jpg";
+		
+		//detectText(filePath);
+	}
+
+	// Detects text in the specified image.
+	public static void detectText(ByteString imgBytes) throws IOException {
+		List<AnnotateImageRequest> requests = new ArrayList<>();
+
+		//ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+		Image img = Image.newBuilder().setContent(imgBytes).build();
+		Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+		AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+		requests.add(request);
+
+		// Initialize client that will be used to send requests. This client only needs
+		// to be created
+		// once, and can be reused for multiple requests. After completing all of your requests, call
+		// the "close" method on the client to safely clean up any remaining background resources.
+		try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+			BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+			List<AnnotateImageResponse> responses = response.getResponsesList();
+
+			for (AnnotateImageResponse res : responses) {
+				if (res.hasError()) {
+					System.out.format("Error: %s%n", res.getError().getMessage());
+					return;
+				}
+
+				// For full list of available annotations, see http://g.co/cloud/vision/docs
+				for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+					System.out.format("Text: %s%n", annotation.getDescription());
+					System.out.format("Position : %s%n", annotation.getBoundingPoly());
+				}
+			}
+		}
+	}
+	
+	
+	public static void detectTextFromImage(String filePath) throws IOException {
+		List<AnnotateImageRequest> requests = new ArrayList<>();
+
+		ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+		Image img = Image.newBuilder().setContent(imgBytes).build();
+		Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+		AnnotateImageRequest request = AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+		requests.add(request);
+
+		// Initialize client that will be used to send requests. This client only needs
+		// to be created
+		// once, and can be reused for multiple requests. After completing all of your requests, call
+		// the "close" method on the client to safely clean up any remaining background resources.
+		try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+			BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+			List<AnnotateImageResponse> responses = response.getResponsesList();
+
+			for (AnnotateImageResponse res : responses) {
+				if (res.hasError()) {
+					System.out.format("Error: %s%n", res.getError().getMessage());
+					return;
+				}
+
+				// For full list of available annotations, see http://g.co/cloud/vision/docs
+				for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+					System.out.format("Text: %s%n", annotation.getDescription());
+					System.out.format("Position : %s%n", annotation.getBoundingPoly());
+				}
+			}
+		}
+	}
+	
+	public String getByteFromMuilpart (MultipartFile file) {
+		return "void";
+	}
+
+}
